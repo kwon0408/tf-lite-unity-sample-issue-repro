3b7440df5aeea7451bab1c3f65895b51c990f87d
J. Kwon <henry.kwon.0408@gmail.com>
2025-08-22 16:03:11 +0900

    add a workaround

--- Assets/SceneScript.cs
+++ Assets/SceneScript.cs
@@ -8,6 +8,7 @@ public class SceneScript: MonoBehaviour
     [SerializeField, FilePopup("*.tflite")]
     private string HorsepowerModelFile;
     private Horsepower Horsepower;
+    
     public void OnClickTestButton()
     {
         var input = new float[] { 6f, 232f, 90f, 3210f, 17.2f, 78f, 0f, 1f, 0f };
@@ -23,6 +24,22 @@ public class SceneScript: MonoBehaviour
         {
             Debug.LogError($"invoke failed: {e.Message}"); // this will hit for second and further inputs
         }
+    }
+    
+    public void OnClickTestButtonWithWorkaround()
+    {
+        var input = new float[] { 6f, 232f, 90f, 3210f, 17.2f, 78f, 0f, 1f, 0f };
+        string path = Path.Combine(Application.streamingAssetsPath, HorsepowerModelFile);
 
+        using var horsepower = new Horsepower(path); // this object will be used only once
+        try
+        {
+            var value = horsepower.Invoke(input);
+            Debug.Log($"invoke success: {value}"); // this will always hit, because it is always the first input
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"invoke failed: {e.Message}"); // this will never hit, because there is no second input
+        }
     }
 }
